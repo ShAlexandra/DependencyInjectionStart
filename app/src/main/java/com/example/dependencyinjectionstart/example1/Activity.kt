2e52198ddf3a2cbd00a3f593ac55ac7143d24e57
ShAlexandra <shalexandra2000@gmail.com>
2022-09-27 16:54:15 +0300

    classes + dagger 2 dependencies

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
@@ -0,0 +1,10 @@
+package com.example.dependencyinjectionstart.example1
+
+class Activity {
+    lateinit var computer: Computer
+    lateinit var keyboard: Keyboard
+
+    init {
+        Component().inject(this)
+    }
+}
